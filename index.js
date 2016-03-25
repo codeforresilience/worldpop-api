456579ba043aa72bb7c52eae17ac466b786293c7
Samweli <smwltwesa6@gmail.com>
2016-03-25 13:31:48 +0300

    for pushing

--- index.js
+++ index.js
@@ -10,11 +10,10 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  var json = JSON.parse('{"count":11,"totalPopulation":12043545.114792835,"totalArea":3576039.0251903664,"polygonArea":3548725.419122968}');
+  response.json(json);
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
